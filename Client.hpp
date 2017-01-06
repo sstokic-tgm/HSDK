d386c07c790b85f3984c48e60ea82f341415d5ed
sstokic-tgm <sstokic@student.tgm.ac.at>
2017-01-06 15:25:52 +0100

    updated the clientclass

--- Client.hpp
+++ Client.hpp
@@ -3,12 +3,21 @@
 #include <Windows.h>
 #include "RecvTable.hpp"
 
+class IClientNetworkable;
+
 class Client
 {
+private:
+
+	using CreateClientClassFn = IClientNetworkable*(*)(int entnum, int serialNum);
+	using CreateEventFn = IClientNetworkable*(*)();
+
 public:
 
-	char *getName();
-	RecvTable *getRecvTable();
-	Client *getNext();
-	int getClsId();
+	CreateClientClassFn m_pCreateFn;
+	CreateEventFn m_pCreateEventFn;
+	char *m_pNetworkName;
+	RecvTable *m_pRecvTable;
+	Client *m_pNext;
+	int m_ClassID;
 };
