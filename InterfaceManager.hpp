4f83d3d1b10267d92a6ea1e4624f276f67f2c101
sstokic-tgm <sstokic@student.tgm.ac.at>
2017-01-06 15:27:44 +0100

    updated InterfaceManager to use the interface reg list

--- InterfaceManager.hpp
+++ InterfaceManager.hpp
@@ -1,37 +1,47 @@
-#pragma once
-
 namespace InterfaceManager
 {
 	class Manager
 	{
 	public:
 
+		/* https://github.com/pmrowla/hl2sdk-csgo/blob/master/public/tier1/interface.h */
+		class InterfaceReg
+		{
+		private:
+
+			using InstantiateInterfaceFn = void*(*)();
+
+		public:
+
+			InstantiateInterfaceFn m_CreateFn;
+			const char *m_pName;
+
+			InterfaceReg *m_pNext;
+		};
+
 		template<typename T>
 		static T *getIface(const char *modName, const char *ifaceName)
 		{
 			T *iface = nullptr;
-			char tmpIface[100];
+			InterfaceReg *ifaceRegList;
+			int partMatchLen = strlen(ifaceName);
 
-			CreateInterfaceFn ifaceFn = (CreateInterfaceFn)GetProcAddress(GetModuleHandleA(modName), "CreateInterface");
-			for (int i = 1; i < 100; ++i)
+			DWORD ifaceFn = reinterpret_cast<DWORD>(GetProcAddress(GetModuleHandleA(modName), "CreateInterface"));
+
+			if (!ifaceFn)
+				return nullptr;
+
+			unsigned int jmpStart = (unsigned int)(ifaceFn)+4;
+			unsigned int jmpTarget = jmpStart + *(unsigned int*)(jmpStart + 1) + 5;
+
+			ifaceRegList = **reinterpret_cast<InterfaceReg***>(jmpTarget + 6);
+
+			for (InterfaceReg *cur = ifaceRegList; cur; cur = cur->m_pNext)
 			{
-				sprintf(tmpIface, "%s0%i", ifaceName, i);
-				iface = (T*)ifaceFn(tmpIface, 0);
-
-				if (iface != nullptr)
-					break;
-
-				sprintf(tmpIface, "%s00%i", ifaceName, i);
-				iface = (T*)ifaceFn(tmpIface, 0);
-
-				if (iface != nullptr)
-					break;
+				if (!strncmp(cur->m_pName, ifaceName, partMatchLen) && strlen(cur->m_pName) - 3 == partMatchLen)
+					iface = reinterpret_cast<T*>(cur->m_CreateFn());
 			}
 			return iface;
 		}
-
-	private:
-
-		using CreateInterfaceFn = void*(*)(const char *name, int *returnC);
 	};
 }
